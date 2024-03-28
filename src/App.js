a9e8d1e47f04c0095fc8749c1e70324171f8107e
Amin Mohammadi <vmp351@hotmail.com>
2024-03-28 11:23:29 +0300

    added tech list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import "./App.css";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 const App = () => {
   useEffect(() => {
@@ -18,6 +22,10 @@ const App = () => {
       <SearchBar />
       <div className="container">
         <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
